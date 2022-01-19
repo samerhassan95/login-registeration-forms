e8e95195f53fe05a723c2790c3a5c159f1776ee0
unknown <arch.samerhassan@gmail.com>
2022-01-19 19:06:58 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import RegisterForm from './Form/registerForm';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoginForm /> */}
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
